273d1e880d29ee305c36d2e84a086381ffefcaba
Waheed Haidaryar <74505893+haidaryar@users.noreply.github.com>
2022-07-02 21:10:23 -0700

    Additional airports added

--- Mapping_Lines_Folders/Mapping_Lines/static/js/logic.js
+++ Mapping_Lines_Folders/Mapping_Lines/static/js/logic.js
@@ -2,30 +2,40 @@
 console.log("working");
 
 // We create the map object with options.
-let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
 
+// Coordinates for each point to be used in the line.
+let line = [
+  [33.9416, -118.4085],
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088],
+// JFK
+  [40.6413, -73.7781],
+  [43.6777, -79.6248]
 
 
+];
 
-// Get data from cities.js
-let cityData = cities;
 
-  // Loop through the cities array and create one marker for each city.
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "yellow"
+}).addTo(map);
 
-  cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-    radius: city.population/100000
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
+//  Add a circle to the map.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+// }).addTo(map);
+
+//  Add a circle to the map
 
 
 
 // We create the tile layer that will be the background of our map.
-let darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let darkmap = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
