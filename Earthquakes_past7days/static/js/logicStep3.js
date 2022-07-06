836fec0923255a08e48a9545556ba8eecbe87683
Waheed Haidaryar <74505893+haidaryar@users.noreply.github.com>
2022-07-06 04:50:43 -0700

    Earthquake layer

---
+++ Earthquakes_past7days/static/js/logicStep3.js
@@ -0,0 +1,142 @@
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+let baseMaps = {
+    "Streets": streets, 
+    "Satellite": satelliteStreets
+
+};
+
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+
+
+L.control.layers(baseMaps).addTo(map);
+
+// let earthQuakeData = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+let torontoHoods = "https://raw.githubusercontent.com/haidaryar/Mapping_Earthquakes/master/torontoNeighborhoods.json";
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    // console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+// L.geoJSON(data).addTo(map);
+function styleInfo(feature) {
+    return {
+      opacity: 1,
+      fillOpacity: 1,
+      fillColor: getColor(feature.properties.mag),
+      color: "#000000",
+      radius: getRadius(feature.properties.mag),
+      stroke: true,
+      weight: 0.5
+
+      
+    };
+  }
+
+
+  // This function determines the color of the circle based on the magnitude of the earthquake.
+function getColor(magnitude) {
+  if (magnitude > 5) {
+    return "#ea2c2c";
+  }
+  if (magnitude > 4) {
+    return "#ea822c";
+  }
+  if (magnitude > 3) {
+    return "#ee9c00";
+  }
+  if (magnitude > 2) {
+    return "#eecc00";
+  }
+  if (magnitude > 1) {
+    return "#d4ee00";
+  }
+  return "#98ee00";
+}
+  // This function determines the radius of the earthquake marker based on its magnitude.
+// Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+function getRadius(magnitude) {
+    if (magnitude === 0) {
+      return 1;
+    }
+    return magnitude * 4;
+  }
+
+// turns each feature into a circle marker
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data, {
+
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(feature, latlng) {
+                console.log(data);
+                return L.circleMarker(latlng);
+            },
+
+
+
+
+            style: styleInfo,
+
+    // We create a popup for each circleMarker to display the magnitude and
+    //  location of the earthquake after the marker has been created and styled.
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
+    }
+
+        }).addTo(map);
+    });
+
+
+
+    //     layer.bindPopup("<h2>" + feature.properties.mag + ", " + feature.properties.place + "</h2>");
+
+    // }
+
+    // }).addTo(map);
+
+  
+
+
+// });
+
+// //------------------------
+// // Grabbing our GeoJSON data.
+// L.geoJSON(airportData, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2>" + feature.properties.city + "</h2>");
+//     }
+
+//   }).addTo(map);
+
+//   L.geoJSON(airportData, {
+//     onEachFeature: function(feature, layer) {
+//         console.log(layer);
+//         layer.bindPopup("<h2>" + feature.properties.city + "</h2>");
+
+//     }
+
+// }).addTo(map);
+
+//https://raw.githubusercontent.com/haidaryar/Mapping_Earthquakes/master/torontoNeighborhoods.json
